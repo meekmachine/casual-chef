803db1570dfdbf4334ddce245e7f63b018d85329
Mapi Pina <mpina09@gmail.com>
2018-07-07 09:26:54 -0400

    working with Router, have to fix homepage component

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,2 +1,15 @@
 import React from "react";
 import "./Homepage.css";
+
+class Homepage extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Casual Chef</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Homepage;
