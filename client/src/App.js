b77e3f1339d509a4a7be50142c7d17efd96e8ed0
Mapi Pina <mpina09@gmail.com>
2018-07-07 10:08:41 -0400

    working on the homepage, trying to fix issue with homepage CSS clashing with index CSS

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="appPage">
         <Nav />
         <Jumbotron />
         <Container>
--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -3,11 +3,30 @@ import "./Homepage.css";
 
 class Homepage extends React.Component {
     render () {
-        return (
-            <div>
-                <h1>Casual Chef</h1>
-            </div>
-        )
+        return <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header className="masthead mb-auto">
+              <div className="inner">
+                <h3 className="masthead-brand">Casual Chef</h3>
+              </div>
+            </header>
+            <main role="main" className="inner cover">
+              <h1 className="cover-heading">
+                Leave the takeout for another day.
+              </h1>
+              <p className="lead">
+                Casual Chef is MERN app that makes it easier for people
+                to meal prep and plan their grocery list. Users will be
+                able to slect which ingredients tickle their fancy, save
+                recipes to their profile, and add ingredients to a
+                shopping list.
+              </p>
+              <p className="lead">
+                <a href="/recipes" className="btn btn-lg btn-secondary">
+                  Learn more
+                </a>
+              </p>
+            </main>
+          </div>;
     }
 }
 
