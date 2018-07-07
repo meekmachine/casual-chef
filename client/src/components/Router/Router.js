7e1240b9428d281805bfea5f2acaf92fd49b60db
Madison Morris <madsmorris14@gmail.com>
2018-07-07 10:30:31 -0400

    auth

--- client/src/components/Router/Router.js
+++ client/src/components/Router/Router.js
@@ -3,11 +3,20 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from "../../App";
 import NotFound from "../NotFound";
 import Homepage from "../Homepage";
+import Login from "../Login";
+import SignUp from "..SignUp";
 
 const Router = () => (
+	state = {
+		loading: true,
+		authenticated: false,
+		user: null
+	}
     <BrowserRouter>
         <Switch>
-            <Route exact path = "/" component={Homepage} />
+            <PrivateRoute exact path = "/" component={Homepage} authenticated={this.state.authenticated}/>
+           	<Route exact path="/login" component={Login} />
+        	<Route exact path="/signup" component={SignUp} />
             <Route path = "/recipes" component={App} />
             <Route component={NotFound} />
         </Switch>
