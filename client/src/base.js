3b386c98cf0de61e8a6da8c95dd8affe5188dc2f
Mapi Pina <mpina09@gmail.com>
2018-07-07 22:29:29 -0400

    solved firebase issue with .env

--- client/src/base.js
+++ client/src/base.js
@@ -1,15 +1,20 @@
-import firebase from 'firebase/app';
+import firebase from "firebase";
 
-var app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
-});
-console.log("KEY");
-console.log(process.env);
-console.log(app);
+const apiKey = `${process.env.REACT_APP_FIREBASE_KEY}`;
+const authDomain = `${process.env.REACT_APP_FIREBASE_DOMAIN}`;
+const databaseURL = `${process.env.REACT_APP_FIREBASE_DATABASE}`;
+const projectId = `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`;
+const storageBucket = `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`;
+const messagingSenderId = `${process.env.REACT_APP_FIREBASE_SENDER_ID}`;
 
-export default app;
+const config = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId
+};
+firebase.initializeApp(config);
+
+export default firebase;
